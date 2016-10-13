63a1ead1f0fcea681253cc56ec7e3a79f9c39729
Adeola Uthman <authman2@gmail.com>
2016-10-13 14:46:22 -0400

    Clean up

---
+++ GraphingCalculator2/src/gc2/main/GraphingCalculator2.java
@@ -0,0 +1,13 @@
+package gc2.main;
+
+public class GraphingCalculator2 {
+
+	public static void main(String[] args) {
+		Polynomial p1 = new Polynomial("9x^3+5x^2-4x-8+9");
+		Polynomial p2 = new Polynomial("x+5");
+		
+		System.out.println("Answer:" + p1.evaluate(1));
+		System.out.println("Answer:" + p2.evaluate(2));
+	}
+
+}
